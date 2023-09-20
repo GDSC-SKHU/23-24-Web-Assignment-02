c51abda013f0d798c8c07adb00ca6a15005ecc69
kdc <kdc9050@naver.com>
2023-09-21 02:08:12 +0900

    2week homework

---
+++ 김동찬/4.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 3번에서 만든 학생 객체를 10개 생성하고, 배열에 집어 넣어라
+ * 
+ * 반복문을 통해, 배열에 모든 학생 객체의 say 함수를 호출하라
+ */
+class Person {
+    say() {
+        console.log('안녕');
+    }
+}
+
+class Student extends Person {
+    say() {
+        console.log("I'm Student!");
+    }
+}
+
+function solution() {
+    const students = []; 
+
+    for (let i = 0; i < 10; i++) {
+        const student = new Student();
+        students.push(student);
+        student.say();
+    }
+
+}
+
+solution();
