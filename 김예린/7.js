1d78cd466aad993d0399da23dae003deaf756db3
yelin <juno2020k@gmail.com>
2023-09-23 01:57:08 +0900

    0923commit

---
+++ 김예린/7.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * 각각 사칙연산을 하는 add, sub, div, mul 함수가 존재 한다.
+ * 
+ * 해당 함수들은 solution 함수의 내부 함수이다.
+ * 
+ * solution 함수에서 인자(매개변수) 2개를 받아 각각 4개 함수를 호출하여 결과를 return 하는 함수를 만들어라
+ * 
+ * solution 함수에서는 4개의 함수를 호출하고, return 된 값을 console.log로 출력하라
+ * 
+ */
+
+function solution(a, b) {
+    function add(a,b){
+        return a+b;
+    }         
+    function sub(a,b){
+        return a-b;
+    }
+    function div(a,b){
+        return a/b;
+    }
+    function mul(a,b){
+        return a*b;
+    }
+    console.log(add(a,b));
+    console.log(sub(a,b));
+    console.log(div(a,b));
+    console.log(mul(a,b));
+}
+
+solution(1, 2);
