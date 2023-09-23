ce1a6a324a628bfbc92541aeb0d50b476e61f876
Park JiSeop <141125424+HarrySeop@users.noreply.github.com>
2023-09-24 01:16:06 +0900

    2주차 과제

---
+++ 박지섭/10.js
@@ -0,0 +1,33 @@
+/**
+ * Person 을 상속받는 본인 이름의 class 를 만들어라
+ * 
+ * 본인 이름의 class 가 생성될때 부모에게 본인 이름과, 나이를 super 로 전달하라
+ * 
+ * 본인 이름의 class 에는 
+ * 
+ * solution 함수에서 본인 이름의 class(객체) 를 생성하고 say 함수를 호출하라
+ */
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    say() {
+        console.log('안녕하세요 제 이름은 ' + this.name + ' 입니다.');
+        console.log('나이는 ' + this.age + ' 입니다.');
+    }
+}
+class ParkJiSeop extends Person {
+    constructor(){
+        super("박지섭", 25);
+    }
+}
+
+function solution() {
+    let parkjiseop = new ParkJiSeop();
+    parkjiseop.say();
+}
+
+solution();
