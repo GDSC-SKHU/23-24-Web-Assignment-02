b634210bf3ca3fd298f02eff4df0816bfe35cb0e
lee, Incheol <dldlscjf1231@naver.com>
2023-09-24 00:59:11 +0900

    과제 수정

--- 과제/1.js
+++ 과제/1.js
@@ -22,15 +22,7 @@
  * *********
  */
 
-function solution(n) {
-    for(let i=0; i<n; i++){
-        for(let j=n-1; j>i; j--){
-            
-        }
-        for(let k=0; k<i; k++){
-            console.log("*")
-        }
-    }    
+function solution(n) {  
 }
-
 solution(3);
+solution(5);
---
+++ 과제/2.js
@@ -0,0 +1,21 @@
+/**
+ * 사람이라는 객체가 있다. 사람이라는 객체가 생성시에, "안녕" 을 출력하라
+ * 
+ * 그리고 사람 이라는 객체의 say 함수를 호출하라
+ * 
+ */
+
+class Person {
+    constructor(){
+        this.message="say";
+    }
+    say() {
+        console.log(this.message);
+    }
+}
+
+
+function solution() {
+}
+
+solution();
