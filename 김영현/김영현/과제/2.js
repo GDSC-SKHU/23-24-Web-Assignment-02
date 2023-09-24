27d2d4c33dc75a5615f225cf83b8d2fa59a6be19
202212025 <dana_03@naver.com>
2023-09-25 00:06:31 +0900

    2_week_hw

---
+++ 김영현/김영현/과제/2.js
@@ -0,0 +1,24 @@
+/**
+ * 사람이라는 객체가 있다. 사람이라는 객체가 생성시에, "안녕" 을 출력하라
+ * 
+ * 그리고 사람 이라는 객체의 say 함수를 호출하라
+ * 
+ */
+
+class Person {
+    say() {
+        console.log('say!');
+    }
+}
+
+function solution() {
+    Person.prototype.say = function() {
+        console.log("안녕");
+      };
+      
+      var person = new Person();
+      
+      person.say();
+}
+
+solution();
